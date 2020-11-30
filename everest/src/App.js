195503fd6449ab0264c79592a536acd063eea1f2
Jonathan <j-m-harrison@outlook.com>
2020-11-30 13:31:43 +0000

    Added app containers for Newsfeed and Stats
    Container design setup, flex weighting and layout for statistics and main app body info in a NewsFeed. Functionality to follow...

--- everest/src/App.js
+++ everest/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header'
+import Newsfeed from './components/Newsfeed/Newsfeed';
+import Stats from './components/Stats/Stats';
 
 function App() {
   return (
     <div className="app">
-      {/* <Header /> */}
+
       <div className='app__header'>
         <Header />
       </div>
       <div className='app__body'>
-
+        <div className='app__container'>
+          <Newsfeed />
+          <Stats />
+        </div>
       </div>
-      {/* <Body /> */}
+
     </div>
   );
 }
