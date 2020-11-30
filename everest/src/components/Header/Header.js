38aec7ae6b856eb578551d619b216bb93488c2c9
Jonathan <j-m-harrison@outlook.com>
2020-11-30 13:08:03 +0000

    Finished up Header
    Completed header basic design, layout for search, links/menu and logo. Functionality to follow...

--- everest/src/components/Header/Header.js
+++ everest/src/components/Header/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import Logo from '../../everest_logo.svg';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className='header__wrapper'>
             {/* Logo */}
             <div className='header__logo'>
-                <img src='https://i.imgur.com/UdiRzau.png' width={50}></img>
+                <img src='https://i.imgur.com/UdiRzau.png' alt='Everest Logo' width={50}></img>
             </div>
             {/* Search */}
+            <div className='header__search'>
+                <div className='header__searchContainer'>
+                    <SearchIcon />
+                    <input placeholder='Search' type='text' />
+                </div>
+            </div>
             {/* Menu Items */}
+            <div class='header__menuItems'>
+                <a href='#'>Free Stocks</a>
+                <a href='#'>Portfolio</a>
+                <a href='#'>Cash</a>
+                <a href='#'>Messages</a>
+                <a href='#'>Account</a>
+            </div>
         </div>
     )
 }
