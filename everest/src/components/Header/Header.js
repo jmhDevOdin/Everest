ed05ae91eb6a231ada94260a88dad9f8b6e692c7
Jonathan <j-m-harrison@outlook.com>
2020-11-30 12:06:54 +0000

    Boiler Plate Cleanup
    Removed template code/react test build and added header component to begin.

---
+++ everest/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header__wrapper'>
+            {/* Logo */}
+            {/* Search */}
+            {/* Menu Items */}
+        </div>
+    )
+}
+
+export default Header;
