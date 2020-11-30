f3b90a21a270ef6df4ffc13d9b64491bf0d2a90c
Jonathan <j-m-harrison@outlook.com>
2020-11-30 14:53:15 +0000

    Added stats component for Stocks
    Stocks on right hand side style prep and base component. Designed and ready for pulled data from API.

--- everest/src/components/Stats/Stats.js
+++ everest/src/components/Stats/Stats.js
@@ -5,7 +5,25 @@ function Stats() {
     return (
         <div className='stats'>
             <div className='stats__container'>
-                
+
+                <div className='stats__header'>
+                    <p>Stocks</p>
+                </div>
+                <div className='stats__content'>
+                    <div className='stats__rows'>
+                        {/* Stocks we can buy */}
+                    </div>
+                </div>
+
+                <div className='stats__header'>
+                    <p>Lists</p>
+                </div>
+                <div className='stats__content'>
+                    <div className='stats__rows'>
+                        {/* Current Stocks */}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
