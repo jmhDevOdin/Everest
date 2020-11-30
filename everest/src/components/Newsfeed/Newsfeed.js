29b305bba66fbced28875e77554cecdc87ab9bde
Jonathan <j-m-harrison@outlook.com>
2020-11-30 14:31:54 +0000

    Imported ChartJS module to mock up line chart
    Line chart mock up for design with psuedo data and styling. Created component and style with ChartJS-2 React Libraries.

--- everest/src/components/Newsfeed/Newsfeed.js
+++ everest/src/components/Newsfeed/Newsfeed.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import LineGraph from '../LineGraph/LineGraph';
 import './Newsfeed.css';
 
 function Newsfeed() {
     return (
         <div className='newsfeed'>
             <div className='newsfeed__container'>
-                
+                <div className='newsfeed__chartSection'>
+                    <div className='newsfeed__portfolio'>
+                        <h1>£85,025.76</h1>
+                        <p>+£115.36 (+0.04%) Today</p>
+                    </div>
+                    <div className='newsfeed__chart'>
+                        <LineGraph />
+                    </div>
+                </div>
             </div>
         </div>
     )
